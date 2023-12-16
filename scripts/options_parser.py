3769c5ed51a9bd05f1d69cdec54abbe90f26d8e2
k4yt3x <i@k4yt3x.com>
2023-12-16 02:35:59 +0000

    feat(options): improved option translation
    Signed-off-by: k4yt3x <i@k4yt3x.com>

--- scripts/options_parser.py
+++ scripts/options_parser.py
@@ -6,6 +6,12 @@ import re
 
 options = {}
 
+with open("data/hoptions.json", "r") as hoptions_file:
+    hoptions = json.load(hoptions_file)
+
+with open("data/translation.json", "r") as translations_file:
+    translations = json.load(translations_file)
+
 # SpecialFeatures.Flashport.FlashRadio.FlashcodeTable
 with open("data/FlashcodeTable.cs", "r", encoding="utf-8") as file:
     for line in file:
@@ -26,9 +32,24 @@ with open("data/FlashcodeTable.cs", "r", encoding="utf-8") as file:
 
             name = arguments[0]
             name = name[1:] if name.startswith("_") else name
+            name = re.sub(r"(?<=\d)_(?=\d)", "/", name)
+            name = re.sub(r"(?<=Rx)_(?=Tx)", "/", name)
             name = name.replace("_", " ")
 
-            options[name.strip()] = {
+            translated = None
+
+            for hoption in hoptions["data"]:
+                if hoption["title"].upper() == name.upper():
+                    translated = hoption["title"]
+                    break
+            else:
+                if translations.get(arguments[0]) is None:
+                    print(f"Untranslated name: '{arguments[0]}' ")
+                    translated = name
+                else:
+                    translated = translations.get(arguments[0])
+
+            options[translated] = {
                 "byte_offset": int(arguments[1]),
                 "bit_offset": int(arguments[2]),
                 "bit_size": int(arguments[3] if len(arguments) >= 4 else "1"),
